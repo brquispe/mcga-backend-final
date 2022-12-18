f1445d4af86f132cee08ef7750e3217dc02aade6
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-12-17 22:48:01 -0300

    BQ-22: Add DELETE method to allow header

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH',
-    'DELETE'
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', '*');
   next();
